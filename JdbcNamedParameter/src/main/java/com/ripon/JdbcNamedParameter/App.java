96d622c51f549e9d872e4ab8577296052516a415
RiponOza <riponoja9@gmail.com>
2020-07-15 09:18:35 -0700

    sdds

---
+++ JdbcNamedParameter/src/main/java/com/ripon/JdbcNamedParameter/App.java
@@ -0,0 +1,13 @@
+package com.ripon.JdbcNamedParameter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
